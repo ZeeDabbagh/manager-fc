88af6dc48af7872483a9d75d9a210b474269f427
Zainab Dabbagh <dabbagh.zainab@gmail.com>
2023-05-01 20:22:52 -0400

    merged conflicts

--- db/schema.sql
+++ db/schema.sql
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-DROP DATABASE IF EXISTS players_db;
-CREATE DATABASE players_db;
-=======
 DROP DATABASE IF EXISTS player_db;
 CREATE DATABASE player_db;
->>>>>>> main
+
--- seeds/index.js
+++ seeds/index.js
@@ -10,14 +10,7 @@ const seedDatabase = async () => {
     individualHooks: true,
     returning: true,
   });
-
-//   for (const project of projectData) {
-//     await Project.create({
-//       ...project,
-//       user_id: users[Math.floor(Math.random() * users.length)].id,
-//     });
-//   }
-
+  
   process.exit(0);
 };
 
