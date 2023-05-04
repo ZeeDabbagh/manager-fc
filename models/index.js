48e742c12c5d251fb7ae85ce60f9c4270c14dffc
Whirlwindraven <fadysherif1998@gmail.com>
2023-05-03 20:16:34 -0400

    updated index.js in models

--- models/index.js
+++ models/index.js
@@ -1,8 +1,20 @@
-
+const Team = require('./team');
+const Player = require('./player');
 const User = require('./User');
 
-module.exports = { User };
+Player.belongsTo(Team, {
+    foreignKey: 'team_id',
+    onDelete: 'CASCADE',
+  });
+  
+  Team.hasMany(Player, {
+    foreignKey: 'team_id',
+    onDelete: 'CASCADE',
+  });
 
+
+module.exports = { User };
+module.exports = { Team, Player };
 const Player = require("./Players");
 
 module.exports = { Player };
