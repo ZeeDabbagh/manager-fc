ad65b39d2e413564732588f93359366ca3d15749
Zainab Dabbagh <dabbagh.zainab@gmail.com>
2023-05-06 16:08:00 -0400

    added logos to teams JSON, fixed login js and routes, created partials

--- public/js/login.js
+++ public/js/login.js
@@ -1,31 +1,117 @@
-// Define an asynchronous function that handles login form submissions
 const loginFormHandler = async (event) => {
-
-  // Prevent the default form submission behavior, which is to reload the page
   event.preventDefault();
 
-  // Get the values of the email and password input fields and trim any leading or trailing whitespace
   const email = document.querySelector('#email-login').value.trim();
   const password = document.querySelector('#password-login').value.trim();
 
-  // If both email and password have values, send an HTTP POST request to the server
   if (email && password) {
-
-    // Send a fetch request to the server with the email and password data in the request body
     const response = await fetch('/api/users/login', {
-      method: 'POST', // Specify that the request is a POST request
-      body: JSON.stringify({ email, password }), // Convert the email and password data to a JSON string and include it in the request body
-      headers: { 'Content-Type': 'application/json' }, // Set the Content-Type header to indicate that the request body is in JSON format
+      method: 'POST',
+      body: JSON.stringify({ email, password }),
+      headers: { 'Content-Type': 'application/json' },
     });
 
-    // If the response status is OK (i.e., the status code is between 200 and 299), redirect the user to the home page
     if (response.ok) {
       document.location.replace('/');
-    } else { // If the response status is not OK, display an alert with an error message
-      alert('Failed to log in');
+    } else {
+      alert('Failed to log in.');
     }
   }
 };
 
-// Add an event listener to the login form that calls the loginFormHandler function when the form is submitted
-document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
+const signupFormHandler = async (event) => {
+  event.preventDefault();
+
+  const username = document.querySelector('#username-signup').value.trim();
+  const email = document.querySelector('#email-signup').value.trim();
+  const password = document.querySelector('#password-signup').value.trim();
+
+  if (username && email && password) {
+    const response = await fetch('/api/users', {
+      method: 'POST',
+      body: JSON.stringify({ username, email, password }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+      document.location.replace('/');
+    } else {
+      alert('Failed to sign up.');
+    }
+  }
+};
+
+document
+  .querySelector('.login-form')
+  .addEventListener('submit', loginFormHandler);
+
+document
+  .querySelector('.signup-form')
+  .addEventListener('submit', signupFormHandler);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // Define an asynchronous function that handles login form submissions
+// const loginFormHandler = async (event) => {
+
+//   // Prevent the default form submission behavior, which is to reload the page
+//   event.preventDefault();
+
+//   // Get the values of the email and password input fields and trim any leading or trailing whitespace
+//   const email = document.querySelector('#email-login').value.trim();
+//   const password = document.querySelector('#password-login').value.trim();
+
+//   // If both email and password have values, send an HTTP POST request to the server
+//   if (email && password) {
+
+//     // Send a fetch request to the server with the email and password data in the request body
+//     const response = await fetch('/api/users/login', {
+//       method: 'POST', // Specify that the request is a POST request
+//       body: JSON.stringify({ email, password }), // Convert the email and password data to a JSON string and include it in the request body
+//       headers: { 'Content-Type': 'application/json' }, // Set the Content-Type header to indicate that the request body is in JSON format
+//     });
+
+//     // If the response status is OK (i.e., the status code is between 200 and 299), redirect the user to the home page
+//     if (response.ok) {
+//       document.location.replace('/');
+//     } else { // If the response status is not OK, display an alert with an error message
+//       alert('Failed to log in');
+//     }
+//   }
+// };
+
+// // Add an event listener to the login form that calls the loginFormHandler function when the form is submitted
+// document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
