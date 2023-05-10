9062ab4b7c7e19c2960a7dcc44cb51f0f2417f6c
Seven <edrezner7@gmail.com>
2023-05-10 18:54:07 -0400

    Reenabled delete player button

--- public/js/deletePlayer.js
+++ public/js/deletePlayer.js
@@ -8,12 +8,6 @@ const closeBtn = modal.querySelector(".close");
 openModalBtn.addEventListener("click", function () {
   modal.style.display = "block";
 
-  var buttonId = this.id;
-  var modalContainer = document.getElementById("modal-container");
-  modalContainer.removeAttribute("class");
-  modalContainer.classList.add(buttonId);
-  document.body.classList.add("modal-active");
-
   const deletePlayerBtn = document.getElementById("deletePlayerBtn");
   deletePlayerBtn.addEventListener("click", deletePlayer);
 });
